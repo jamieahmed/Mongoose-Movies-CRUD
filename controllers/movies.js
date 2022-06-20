e8fc71082973dbc3ee43b228628c2df949cabdd3
Jamie Ahmed <jamieahmed273@gmail.com>
2022-06-20 13:21:06 -0400

    addd CRUD

--- controllers/movies.js
+++ controllers/movies.js
@@ -13,32 +13,85 @@ function create(req, res) {
     req.body.cast = req.body.cast.split(", ")
   }
   for (let key in req.body) {
-	  if (req.body[key] === '') delete req.body[key]
-	}
+    if (req.body[key] === '') delete req.body[key]
+  }
   Movie.create(req.body)
-  .then(movie => {
-    console.log("CREATED MOVIE:", movie)
-    // SEND A GET REQUEST TO THIS URL!
-    res.redirect(`/movies`)
-  })
-  .catch(err => {
-    console.log(err)
-    res.redirect('/movies')
-  })
+    .then(movie => {
+      console.log("CREATED MOVIE:", movie)
+      // SEND A GET REQUEST TO THIS URL!
+      res.redirect(`/movies`)
+    })
+    .catch(err => {
+      console.log(err)
+      res.redirect('/movies')
+    })
 }
 
 function index(req, res) {
   Movie.find({})
-  .then(movies => {
-    res.render("movies/index", {
-      movies: movies,
-      title: "All Movies",
+    .then(movies => {
+      res.render("movies/index", {
+        movies: movies,
+        title: "All Movies",
+      })
     })
-  })
 }
-
+function show(req, res) {
+  Movie.findById(req.params.id)
+    .then(movie => {
+      res.render('movies/show', {
+        title: 'Movie Detail',
+        movie: movie,
+      })
+    })
+    .catch(err => {
+      console.log(err)
+      res.redirect("/")
+    })
+}
+function deleteMovie(req, res) {
+  Movie.findByIdAndDelete(req.params.id)
+    .then(() => {
+      res.redirect("/movies")
+    })
+    .catch(err => {
+      console.log(err)
+      res.redirect("/")
+    })
+}
+function edit(req, res) {
+  Movie.findById(req.params.id)
+    .then(movie => {
+      res.render("movies/edit", {
+        movie, // same as: movie: movie
+        title: "Edit Movie"
+      })
+    })
+    .catch(err => {
+      console.log(err)
+      res.redirect("/")
+    })
+}
+function update(req, res) {
+  req.body.nowShowing = !!req.body.nowShowing
+  for (let key in req.body) {
+    if (req.body[key] === "") delete req.body[key]
+  }
+  Movie.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    .then(movie => {
+      res.redirect(`/movies/${movie._id}`)
+    })
+    .catch(err => {
+      console.log(err)
+      res.redirect("/")
+    })
+}
 export {
   newMovie as new,
   create,
   index,
+  show,
+  deleteMovie as delete,
+  edit,
+  update
 }
--- routes/movies.js
+++ routes/movies.js
@@ -12,6 +12,17 @@ router.get('/new', moviesCtrl.new)
 // POST localhost:3000/movies
 router.post('/', moviesCtrl.create)
 
+// Show localhost:3000/movies/:id
+router.get('/:id', moviesCtrl.show)
+
+// delete localhost:3000/movies/:id
+router.delete("/:id", moviesCtrl.delete)
+
+// localhost:3000/movies/:id/edit
+router.get("/:id/edit", moviesCtrl.edit)
+
+// localhost:3000/movies/:id
+router.put("/:id", moviesCtrl.update)
 export {
   router
 }
